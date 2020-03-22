ef3521116cfb98f00ff9b8ca652141d52939b0c8
Rick708 <Rick708@users.noreply.github.com>
2020-03-22 22:01:46 +0900

    修正

---
+++ Assets/PlayerHP.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text HPText = GetComponent<Text>();
+        HPText.text = "100";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
